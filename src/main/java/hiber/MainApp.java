b47bb540e06994a499a9fb157366e351be42b15b
folkwolf <ibragimoffer@yandex.ru>
2021-12-24 00:41:33 +0500

    push

---
+++ src/main/java/hiber/MainApp.java
@@ -0,0 +1,42 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class MainApp {
+   public static void main(String[] args) throws SQLException {
+      AnnotationConfigApplicationContext context = 
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
+      UserService userService = context.getBean(UserService.class);
+
+      User user = new User("User1", "Lastname1", "user1@mail.ru");
+      Car car = new Car("Honda", 124);
+      user.setCar(car);
+
+      userService.add(user);
+
+
+//      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
+//      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
+//      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+
+      List<User> users = userService.listUsers();
+      for (User userr : users) {
+         System.out.println("Id = "+userr.getId());
+         System.out.println("First Name = "+userr.getFirstName());
+         System.out.println("Last Name = "+userr.getLastName());
+         System.out.println("Email = "+userr.getEmail());
+         System.out.println("Car = " + userr.getCar());
+         System.out.println();
+      }
+
+      context.close();
+   }
+}
---
+++ src/main/java/hiber/model/Car.java
@@ -0,0 +1,58 @@
+package hiber.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="cars")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="model", nullable = false)
+    private String model;
+
+    @Column(name="series", nullable = false)
+    private int series;
+
+    public Car(String model, int series) {
+        this.model = model;
+        this.series = series;
+    }
+
+    public Car() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", series=" + series +
+                '}';
+    }
+}
